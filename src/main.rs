223b9b2d50c22efc6855e5836cffa0ecddaaa2e5
Advait Suresh <avtss.sonu@gmail.com>
2024-08-30 15:38:37 +0530

    add dbus for session manager

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,63 @@
+// 72
+
+use chrono::{prelude::DateTime, Local};
 use futures_util::stream::StreamExt;
+use std::{
+    time::{Duration, Instant, SystemTime, UNIX_EPOCH},
+    usize,
+};
 use zbus::Connection;
 use zbus_macros::proxy;
 
 #[proxy(
-    default_service = "org.gnome.ScreenSaver",
-    default_path = "/org/gnome/ScreenSaver",
-    interface = "org.gnome.ScreenSaver"
+    default_service = "org.gnome.SessionManager",
+    default_path = "/org/gnome/SessionManager/Presence",
+    interface = "org.gnome.SessionManager.Presence"
 )]
-trait ScreenSaver {
+trait SessionManager {
     #[zbus(signal)]
-    fn active_changed(&self, active_changed: bool) -> zbus::Result<()>;
+    fn status_changed(&self, status: u32) -> zbus::Result<()>;
+}
+
+#[derive(Debug)]
+struct Timer {
+    timestamp: u128,
+}
+
+impl Timer {
+    fn update(&mut self, new_value: u128) {
+        self.timestamp = new_value;
+    }
+
+    fn calculate_diff(&self, current_time: u128) -> u128 {
+        current_time - self.timestamp
+    }
 }
 
 #[tokio::main]
 async fn main() -> zbus::Result<()> {
-    let connection = Connection::session().await?;
-    let screen_saver_proxy = ScreenSaverProxy::new(&connection).await?;
-    let mut screan_saver_stream = screen_saver_proxy.receive_active_changed().await?;
+    // est connections
+    let session_connection = Connection::session().await?;
 
-    while let Some(msg) = screan_saver_stream.next().await {
-        let args: ActiveChangedArgs = msg.args().expect("Error while parsing message");
-        println!("Got new message boys: status:{}", args.active_changed)
+    // setup proxy with the connections
+    let session_manager_proxy = SessionManagerProxy::new(&session_connection).await?;
+
+    let mut session_manager_stream = session_manager_proxy.receive_status_changed().await?;
+
+    let some_time = Local::now();
+    println!("is this working {:?}", some_time);
+
+    while let Some(signal) = session_manager_stream.next().await {
+        let args: StatusChangedArgs = signal.args().expect("Error parsing the args");
+        match args.status {
+            0 => {
+                println!("log back in time: {:?}", Instant::now())
+            }
+            3 => {
+                println!("logout time: {:?}", Instant::now())
+            }
+            _ => println!("ignore me man"),
+        }
     }
 
     Ok(())
